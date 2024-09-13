f470a98de29948c32d0eb1adf2977e35f1c91445
Dev Singh <dsingh14@illinois.edu>
2024-09-13 17:10:00 -0500

    update default in prod to 36500 days

--- machine_controller/prod/create_prod_client_cert.sh
+++ machine_controller/prod/create_prod_client_cert.sh
@@ -3,5 +3,5 @@
 read -p "Enter service name: " user
 openssl genrsa -out client.key 4096
 openssl req -new -key client.key -out client.csr -subj "/C=US/ST=Illinois/L=Urbana/O=University of Illinois Urbana-Champaign/OU=Association for Computing Machinery - Production/CN=$user"
-openssl x509 -req -in client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client.crt -days 3650 -sha256
+openssl x509 -req -in client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client.crt -days 36500 -sha256
 rm client.csr ca.srl
